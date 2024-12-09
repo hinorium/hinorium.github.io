d64451519eb33a0cd1c42b7e57f5e757f00fdd37
hinorium. <ldplayers620@gmail.com>
2024-12-09 13:38:43 +0900

    Create page-2861645631c11887.js

---
+++ _next/static/chunks/app/page-2861645631c11887.js
@@ -0,0 +1,369 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[931], {
+    7659: function(e, t, s) {
+        Promise.resolve().then(s.bind(s, 3504))
+    },
+    3504: function(e, t, s) {
+        "use strict";
+        s.r(t),
+        s.d(t, {
+            default: function() {
+                return d
+            }
+        });
+        var i = s(7437)
+          , a = s(5601)
+          , r = s.n(a)
+          , n = s(703)
+          , o = s(2265)
+          , c = s(3633)
+          , l = s.n(c);
+        function d() {
+            return (0,
+            i.jsxs)(i.Fragment, {
+                children: [(0,
+                i.jsxs)(r(), {
+                    children: [(0,
+                    i.jsx)("title", {
+                        children: "Wave - Ride the wave"
+                    }), (0,
+                    i.jsx)("link", {
+                        rel: "icon",
+                        href: "/favico.ico",
+                        type: "image/x-icon"
+                    }), (0,
+                    i.jsx)("meta", {
+                        name: "viewport",
+                        content: "width=device-width, initial-scale=1.0"
+                    }), (0,
+                    i.jsx)("meta", {
+                        name: "title",
+                        content: "Wave"
+                    }), (0,
+                    i.jsx)("meta", {
+                        name: "description",
+                        content: "The best free exploit for ROBLOX."
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "og:type",
+                        content: "website"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "og:url",
+                        content: "https://getwave.gg"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "og:title",
+                        content: "Wave"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "og:description",
+                        content: "The best free exploit for ROBLOX."
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "og:image",
+                        content: "https://i.imgur.com/UJPRAkn.png"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "twitter:card",
+                        content: "summary_large_image"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "twitter:url",
+                        content: "https://getwave.gg"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "twitter:title",
+                        content: "Wave"
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "twitter:description",
+                        content: "The best free exploit for ROBLOX."
+                    }), (0,
+                    i.jsx)("meta", {
+                        property: "twitter:image",
+                        content: "https://i.imgur.com/UJPRAkn.png"
+                    })]
+                }), (0,
+                i.jsxs)("div", {
+                    className: "bg-background bg-[url('/assets/bg.svg')] bg-cover text-white",
+                    children: [(0,
+                    i.jsxs)("div", {
+                        className: "h-[512px] bg-[#112F49] bg-[url('/assets/ride.png')] bg-cover border border-[#475E8299] bg-fixed overflow-hidden relative",
+                        children: [(0,
+                        i.jsxs)("div", {
+                            className: "p-12 flex items-center z-auto justify-end",
+                            children: [(0,
+                            i.jsx)("div", {
+                                children: (0,
+                                i.jsx)(n.default, {
+                                    onClick: () => window.location = "/",
+                                    src: "/assets/Wave.png",
+                                    alt: "Wave Logo",
+                                    width: 80,
+                                    height: 80,
+                                    className: "cursor-pointer"
+                                })
+                            }), (0,
+                            i.jsx)("div", {
+                                className: "ml-auto",
+                                children: (0,
+                                i.jsxs)("div", {
+                                    className: "ml-8 hidden md:flex",
+                                    children: [(0,
+                                    i.jsx)("a", {
+                                        className: "mr-4 cursor-pointer",
+                                        children: "HOME"
+                                    }), (0,
+                                    i.jsx)("a", {
+                                        href: "https://duckys-playground.gitbook.io/wave/getting-started",
+                                        className: "mr-4",
+                                        children: "DOCUMENTATION"
+                                    }), (0,
+                                    i.jsx)("a", {
+                                        href: "#features",
+                                        className: "mr-4",
+                                        children: "FEATURES"
+                                    }), (0,
+                                    i.jsx)("a", {
+                                        href: "/resellers",
+                                        className: "mr-4 font-bold",
+                                        children: "PURCHASE"
+                                    }), (0,
+                                    i.jsx)("a", {
+                                        href: "https://discord.getwave.gg",
+                                        className: "mr-4 font-bold",
+                                        children: "DISCORD"
+                                    })]
+                                })
+                            })]
+                        }), (0,
+                        i.jsx)("div", {
+                            className: "p-4 flex items-center justify-center",
+                            children: (0,
+                            i.jsxs)("div", {
+                                className: "text-center",
+                                children: [(0,
+                                i.jsx)("h1", {
+                                    className: "text-6xl font-bold italic mb-6",
+                                    children: "Let the WAVE begin"
+                                }), (0,
+                                i.jsxs)("div", {
+                                    children: [(0,
+                                    i.jsx)("button", {
+                                        onClick: () => window.location = "https://cdn.getwave.gg/userinterface/Wave-Setup.exe",
+                                        className: "border mt-1 border-white bg-white text-black font-bold text-xl rounded-lg p-4 w-52 mr-2 transition hover:",
+                                        children: "Download"
+                                    }), (0,
+                                    i.jsx)("button", {
+                                        onClick: () => window.location = "https://key.getwave.gg",
+                                        className: "border mt-1 border-white bg-white text-black font-bold text-xl rounded-lg p-4 w-52 mr-2 transition hover:",
+                                        children: "Get Key"
+                                    })]
+                                }), (0,
+                                i.jsx)("p", {
+                                    className: "italic opacity-50 mt-4",
+                                    children: "Brought to you by the Wave Development Team"
+                                })]
+                            })
+                        })]
+                    }), (0,
+                    i.jsxs)("div", {
+                        className: "text-center",
+                        children: [(0,
+                        i.jsx)("p", {
+                            className: "text-2xl italic opacity-60 font-light mt-4",
+                            children: "Download for..."
+                        }), (0,
+                        i.jsxs)("div", {
+                            className: "flex justify-center",
+                            children: [(0,
+                            i.jsxs)("div", {
+                                id: "windows",
+                                onClick: () => window.location = "https://cdn.getwave.gg/userinterface/Wave-Setup.exe",
+                                "data-tilt": !0,
+                                "data-tilt-scale": "1.1",
+                                className: "cursor-pointer drop-shadow-lg mt-8 w-64 h-80 bg-[#0F172499] backdrop-blur bg-[url('/assets/CardBG.svg')] rounded-lg border border-[#355168] m-4",
+                                children: [(0,
+                                i.jsx)("p", {
+                                    className: "pt-8 text-2xl font-light",
+                                    children: "Windows"
+                                }), (0,
+                                i.jsx)(n.default, {
+                                    src: "/assets/Windows.svg",
+                                    alt: "Windows Logo",
+                                    width: 64,
+                                    height: 64,
+                                    className: "mx-auto my-auto mt-16"
+                                })]
+                            }), (0,
+                            i.jsxs)("div", {
+                                id: "ios",
+                                "data-tilt": !0,
+                                "data-tilt-scale": "1.1",
+                                className: "cursor-pointer drop-shadow-lg mt-8 w-64 h-80 bg-[#0F172499] backdrop-blur bg-[url('/assets/CardBG.svg')] rounded-lg border border-[#355168] m-4",
+                                children: [(0,
+                                i.jsx)("p", {
+                                    className: "pt-8 text-2xl font-light",
+                                    children: "iOS"
+                                }), (0,
+                                i.jsx)("p", {
+                                    className: "text-center italic opacity-55",
+                                    children: "Coming soon..."
+                                }), (0,
+                                i.jsx)(n.default, {
+                                    src: "/assets/Apple.svg",
+                                    alt: "Apple Logo",
+                                    width: 64,
+                                    height: 64,
+                                    className: "mx-auto my-auto mt-8"
+                                })]
+                            })]
+                        })]
+                    }), (0,
+                    i.jsx)("div", {
+                        className: "text-center justify-center",
+                        children: (0,
+                        i.jsx)(n.default, {
+                            className: "m-auto mt-4",
+                            src: "/assets/Splitter.png",
+                            alt: "Splitter",
+                            width: 64,
+                            height: 64
+                        })
+                    }), (0,
+                    i.jsxs)("div", {
+                        className: "text-center flex flex-wrap justify-center",
+                        id: "features",
+                        children: [(0,
+                        i.jsx)(h, {
+                            icon: "/assets/icons/key.svg",
+                            title: "Secure",
+                            description: "We make sure your time using our exploit is secure and undetected."
+                        }), (0,
+                        i.jsx)(h, {
+                            icon: "/assets/icons/idk.svg",
+                            title: "Unrestricted Execution",
+                            description: "Featuring 100% UNC for premium users, Wave is able to run any script on the market that you may throw at it"
+                        }), (0,
+                        i.jsx)(h, {
+                            icon: "/assets/icons/star.svg",
+                            title: "AI Integration",
+                            description: "Got a bug in your code and don't know how to fix it? WaveAI can help you!"
+                        })]
+                    }), (0,
+                    i.jsxs)("div", {
+                        className: "text-center flex flex-wrap justify-center",
+                        children: [(0,
+                        i.jsx)(h, {
+                            icon: "/assets/icons/list.svg",
+                            title: "Script Hub",
+                            description: "Integratd Script Hub right within the UI so you never need to go searching online for scripts again!"
+                        }), (0,
+                        i.jsx)(h, {
+                            icon: "/assets/icons/exec.svg",
+                            title: "Decompiler",
+                            description: "Wave features a state-of-the-art decompiler, being able to decompile any Local/Module script that you may need"
+                        }), (0,
+                        i.jsx)(h, {
+                            icon: "/assets/icons/ticket.svg",
+                            title: "Free!",
+                            description: "And the best part? Wave is free! By clicking the 'Get Key' button at the top of the page, you can get a key you can use on wave"
+                        })]
+                    }), (0,
+                    i.jsx)("div", {
+                        className: "text-center justify-center",
+                        children: (0,
+                        i.jsx)(n.default, {
+                            className: "m-auto mt-4",
+                            src: "/assets/Splitter.png",
+                            alt: "Splitter",
+                            width: 64,
+                            height: 64
+                        })
+                    }), (0,
+                    i.jsxs)("div", {
+                        className: "text-center",
+                        children: [(0,
+                        i.jsx)("p", {
+                            className: "text-2xl italic opacity-60 font-light mt-4",
+                            children: "Preview"
+                        }), (0,
+                        i.jsxs)("div", {
+                            className: "flex flex-wrap justify-center",
+                            children: [(0,
+                            i.jsx)(n.default, {
+                                "data-tilt": !0,
+                                className: "m-4 drop-shadow-lg h-auto",
+                                src: "/assets/showcase1.png",
+                                alt: "Showcase 1",
+                                width: 400,
+                                height: 200
+                            }), (0,
+                            i.jsx)(n.default, {
+                                "data-tilt": !0,
+                                className: "m-4 drop-shadow-lg h-auto",
+                                src: "/assets/showcase2.png",
+                                alt: "Showcase 2",
+                                width: 400,
+                                height: 200
+                            })]
+                        })]
+                    }), (0,
+                    i.jsx)("footer", {
+                        children: (0,
+                        i.jsx)("div", {
+                            className: "h-16 bg-[#0F171E11] backdrop-blur-lg flex border border-[#475E8299] items-center justify-center",
+                            children: (0,
+                            i.jsx)("p", {
+                                className: "text-center italic opacity-60",
+                                children: "\xa9 2024 Wave Development Team"
+                            })
+                        })
+                    })]
+                })]
+            })
+        }
+        function h(e) {
+            let {icon: t, title: s, description: a} = e
+              , r = (0,
+            o.useRef)(null);
+            return (0,
+            o.useEffect)( () => {
+                r.current && l().init(r.current, {
+                    scale: 1.1
+                })
+            }
+            , []),
+            (0,
+            i.jsxs)("div", {
+                ref: r,
+                className: "drop-shadow-lg w-96 h-64 bg-[#0F172499] bg-[url('/assets/CardBG.svg')] m-4 mt-8 backdrop-blur rounded-lg border border-[#355168] bg-cover",
+                children: [(0,
+                i.jsxs)("div", {
+                    className: "flex items-center text-2xl font-bold",
+                    children: [(0,
+                    i.jsx)(n.default, {
+                        className: "w-12 h-12 m-4",
+                        src: t,
+                        alt: s,
+                        width: 48,
+                        height: 48
+                    }), (0,
+                    i.jsx)("p", {
+                        children: s
+                    })]
+                }), (0,
+                i.jsx)("p", {
+                    className: "text-xl text-left ml-8 mr-8",
+                    children: a
+                })]
+            })
+        }
+    }
+}, function(e) {
+    e.O(0, [202, 971, 69, 744], function() {
+        return e(e.s = 7659)
+    }),
+    _N_E = e.O()
+}
+]);
